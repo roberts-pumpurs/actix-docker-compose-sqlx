629eae066d195a754e1278cd1d027ec1a2e5d48f
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-01-24 17:34:24 +0200

    Removed cargo watch

--- actix-backend/init.sh
+++ actix-backend/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # While developing, we want to keep the database state up-to date at all times
+
+# Kill previous instance of script
+killall -s 9 actix-mysql-sql
 cargo sqlx prepare
 cargo run
